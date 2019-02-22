7ace2c58f643753488cd244c4522cea87cbef0cb
clamtian <luckytsw@163.com>
2019-02-22 16:30:13 +0800

    20190222

--- src/main/java/com/mmall/dao/CartMapper.java
+++ src/main/java/com/mmall/dao/CartMapper.java
@@ -2,6 +2,8 @@ package com.mmall.dao;
 
 import com.mmall.pojo.Cart;
 
+import java.util.List;
+
 public interface CartMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,17 @@ public interface CartMapper {
     int updateByPrimaryKeySelective(Cart record);
 
     int updateByPrimaryKey(Cart record);
+
+    List selectCartByUserId(Integer userId);
+
+    int selectCartProductCheckedStatusByUserId(Integer userId);
+
+    Cart selectCartByUserIdProductId(Integer userId, Integer productId);
+
+    int deleteByUserIdProductIds(Integer userId,List productList);
+
+    int checkedOrUncheckedProduct(Integer userId,Integer productId,Integer checked);
+
+    int selectCartProductCount(Integer userId);
+
 }
